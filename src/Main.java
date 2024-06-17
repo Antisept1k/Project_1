bfb538569f013ea6885d5527e9b9a021a64b9b8b
Ivan <onesock2007@gmail.com>
2024-06-17 15:44:02 +0300

    "Req_1 Base authentication"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-import  java.util.Scanner;
+import java.util.Scanner;
+
 public class Main {
 
+    static String Password = "password";
+    static Scanner scanner = new Scanner(System.in);
 
-
-
+    public static void main(String[] args) {
+        run();
+    }
+    static void run(){
+        boolean PassCheck = false;
+        for (int i = 0; i<3;i++){
+            System.out.println(" your password:");
+            String input = scanner.nextLine();
+            if (Password.equals(input)){
+                PassCheck = true;
+                break;
+            }
+            System.out.println("access denied, the password was wrong.");
+        }
+        System.out.println(PassCheck ? "Welcome to the application" : "aplplication was blocked");
+    }
 }
